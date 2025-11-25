a9459701ad640ae477aacedbf5ad56bbb8c1ab40
JibrilOla <iam.olajire@gmail.com>
2025-11-25 09:12:16 +0100

    remove django media

--- campusfeed_project/settings.py
+++ campusfeed_project/settings.py
@@ -114,7 +114,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
